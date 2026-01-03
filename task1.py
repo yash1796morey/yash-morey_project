96bdc1fc43cb372ac061ff398a4c59609358d012
yash1796morey <yashmorey95@gmail.com>
2026-01-03 19:10:58 +0530

    task 1 completed

---
+++ task1.py
@@ -0,0 +1,21 @@
+# Take two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Division with check
+# if num2 != 0:
+#     division = num1 / num2
+# else:
+#     division = "Cannot divide by zero"
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
