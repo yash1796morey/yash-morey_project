f57725fc11489fa9645b8826e1e7abab38b8ec4f
yash1796morey <yashmorey95@gmail.com>
2026-01-03 19:18:06 +0530

    task 2 completed

---
+++ task2.py
@@ -0,0 +1,9 @@
+# Take first name and last name from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print("Hello,", full_name + "! Welcome to Python programming.")
